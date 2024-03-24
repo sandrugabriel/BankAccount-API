716b39648e00e22e416c5618a7180b0ff393466b
sandrugabriel <sandru11.gabriel@gmail.com>
2024-03-24 21:31:23 +0200

    gitignore

---
+++ BankAccountAPI/Repository/interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using BankAccountAPI.Models;
+using System;
+
+namespace BankAccountAPI.Repository.interfaces
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<BankAccount>> GetAllAsync();
+
+        Task<List<BankAccount>> GetByOwnerName(string name);
+
+    }
+}
